4ed70973c28854f9dff052f7342f1fae7bbdce78
erubinst <erubinst@oberlin.edu>
2025-10-24 16:18:31 -0400

    adding config file

---
+++ locations/config.py
@@ -0,0 +1,6 @@
+
+DATA_FOLDER = '/Users/esmerubinstein/Desktop/ICLL/AICaring/AICaring/location_data'
+WEEKS = ['A']
+OUTPUT_FOLDER = '/Users/esmerubinstein/Desktop/ICLL/AICaring/AICaring/labeled_locations/'
+API_KEY = 'AIzaSyComIKziB9u4r95SVGFIYk6RcPpiFmY-Jg'
+ABSOLUTE_PATH = '/Users/esmerubinstein/Desktop/ICLL/AICaring/AICaring'
